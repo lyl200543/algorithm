c85a2f0ee726d72018de056c94f6574f8e2b18cc
lyl <13416088820@163.com>
2025-04-24 21:42:06 +0800

    4-24

--- List/src/leetcode_203.java
+++ List/src/leetcode_203.java
@@ -9,3 +9,37 @@
 public class leetcode_203 {
 
 }
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode () {
+    }
+
+    ListNode (int val) {
+        this.val = val;
+    }
+
+    ListNode (int val , ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode removeElements (ListNode head , int val) {
+        ListNode node = new ListNode();
+        node.next = head;
+        ListNode newhead = node;
+        if (head != null) {
+            while (node.next != null) {
+                if (node.next.val == val)
+                    node.next = node.next.next;
+                else
+                    node = node.next;
+            }
+        }
+        return newhead.next;
+    }
+}
