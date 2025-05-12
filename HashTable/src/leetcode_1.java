f43714adc54b9bbc10270cb7fb4bdcd7cf29b163
lyl <13416088820@163.com>
2025-05-12 21:47:10 +0800

    5-12

--- HashTable/src/leetcode_1.java
+++ HashTable/src/leetcode_1.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 /**
  *ClassName: leetcode_1
  *Package: PACKAGE_NAME
@@ -6,4 +8,27 @@
  *@Version 1.0
  */
 public class leetcode_1 {
+}
+
+class Solution4 {
+    public int[] twoSum (int[] nums , int target) {
+        int[] ret = new int[2];
+        HashSet<Integer> set = new HashSet<>();
+        int i, j, k = 0;
+        for (i = 0; i < nums.length ; i++) {
+            set.add(nums[i]);
+        }
+        for (i = 0; i < nums.length ; i++) {
+            int num = target - nums[i];
+            if (set.contains(num)) {
+                for (j = 0; j < nums.length ; j++) {
+                    if (num == nums[j]) {
+                        ret[k++] = j;
+                        nums[j] = Integer.MAX_VALUE;
+                    }
+                }
+            }
+        }
+        return ret;
+    }
 }
