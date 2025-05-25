2899da294d5dd16a7c146fb5628493afed9ac341
lyl <13416088820@163.com>
2025-05-25 21:44:53 +0800

    5-25

--- HashTable/src/leetcode_15.java
+++ HashTable/src/leetcode_15.java
@@ -1,4 +1,5 @@
-import java.util.List;
+import javax.swing.*;
+import java.util.*;
 
 /**
  *ClassName: leetcode_15
@@ -11,7 +12,59 @@ public class leetcode_15 {
 }
 
 class Solution7 {
-    public List<List<Integer>> threeSum(int[] nums) {
-        
+    public List<List<Integer>> threeSum (int[] nums) {
+//        Set<Integer> set = new HashSet<>();
+//        List<List<Integer>> lists = new ArrayList<>();
+//        for (int i = 0 ; i < nums.length ; i++) {
+//            set.add(nums[i]);
+//        }
+//        for (int i = 0 ; i < nums.length ; i++) {
+//            for (int j = i + 1 ; j < nums.length ; j++) {
+//                if (set.contains(- (nums[i] + nums[j]))) {
+//                    List<Integer> list = new ArrayList<>();
+//                    list.add(nums[i]);
+//                    list.add(nums[j]);
+//                    list.add(- (nums[i] + nums[j]));
+//                    lists.add(list);
+//                }
+//            }
+//        }
+//        return lists;
+
+
+        //使用哈希法，去重操作交麻烦（未尝试）
+        //双指针法：
+        List<List<Integer>> lists = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0 ; i < nums.length ; i++) {
+            //剪枝操作
+            if (nums[i] > 0)
+                break;
+            //对a去重
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0)
+                    right--;
+                else if (sum < 0)
+                    left++;
+                else {
+                    lists.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
+                    left++;
+                    right--;
+                    //b去重
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    //c去重
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                }
+            }
+        }
+        return lists;
+
     }
 }
