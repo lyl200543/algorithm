0859c08d16ce07b00066c997a8d50fc2fc69e30c
lyl <13416088820@163.com>
2025-05-27 21:43:38 +0800

    5-27

--- HashTable/src/leetcode_18.java
+++ HashTable/src/leetcode_18.java
@@ -10,6 +10,11 @@ import java.util.List;
  *@Version 1.0
  */
 public class leetcode_18 {
+    public static void main (String[] args) {
+        int[] nums = new int[]{4 , - 9 , - 2 , - 2 , - 7 , 9 , 9 , 5 , 10 , - 10 , 4 , 5 , 2 , - 4 , - 2 , 4 , - 9 , 5};
+        Solution8 solution8 = new Solution8();
+        System.out.println(solution8.fourSum(nums , - 13));
+    }
 }
 
 class Solution8 {
@@ -17,14 +22,14 @@ class Solution8 {
         List<List<Integer>> lists = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0 ; i < nums.length ; i++) {
-            if (nums[i] > target)
-                return lists;
+//            if (nums[i] > target)
+//                return lists;
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
             int goal = target - nums[i];
             for (int j = i + 1 ; j < nums.length ; j++) {
-                if (nums[j] > goal)
-                    return lists;
+//                if (nums[j] > goal)
+//                    break;
                 if (j > (i + 1) && nums[j] == nums[j - 1])
                     continue;
                 int left = j + 1;
@@ -36,17 +41,18 @@ class Solution8 {
                     else if (sum > goal)
                         right--;
                     else {
-                        lists.add(Arrays.asList(nums[j] , nums[left] , nums[right]));
+                        lists.add(Arrays.asList(nums[i] , nums[j] , nums[left] , nums[right]));
                         left++;
                         right--;
-                        if (left < right && nums[left] == nums[left - 1])
+                        while (left < right && nums[left] == nums[left - 1])
                             left++;
-                        if (left < right && nums[right] == nums[right + 1])
-                            left--;
+                        while (left < right && nums[right] == nums[right + 1])
+                            right--;
                     }
                 }
             }
         }
         return lists;
     }
+
 }
