13e77fe29a652ab2130c95d066da882b05282a64
lyl <13416088820@163.com>
2025-06-03 21:38:19 +0800

    6-3

--- String/src/leetcode_28.java
+++ String/src/leetcode_28.java
@@ -33,21 +33,47 @@ class Solution4 {
 //        return - 1;
 
         //KMP算法：匹配字符串
-        String str = needle + "#" + haystack;
-        int[] next = new int[str.length()];
+//        String str = needle + "#" + haystack;
+//        int[] next = new int[str.length()];
+//        next[0] = 0;
+//        for (int i = 1 ; i < str.length() ; i++) {
+//            int len = next[i - 1];
+//            while (len > 0 && str.charAt(len) != str.charAt(i)) {
+//                len = next[len - 1];
+//            }
+//            if (str.charAt(len) == str.charAt(i)) {
+//                next[i] = len + 1;
+//                if (next[i] == needle.length())
+//                    return i - needle.length() * 2;
+//            }
+//        }
+//        return - 1;
+
+
+        //模板：
+        if (needle.length() == 0)
+            return 0;
+        int[] next = new int[needle.length()];
         next[0] = 0;
-        for (int i = 1 ; i < str.length() ; i++) {
-            int len = next[i - 1];
-            while (len > 0 && str.charAt(len) != str.charAt(i)) {
-                len = next[len - 1];
-            }
-            if (str.charAt(len) == str.charAt(i)) {
-                next[i] = len + 1;
-                if (next[i] == needle.length())
-                    return i - needle.length() * 2;
+        int j = 0;
+        for (int i = 1 ; i < needle.length() ; i++) {
+            while (j > 0 && needle.charAt(j) != needle.charAt(i))
+                j = next[j - 1];
+            if (needle.charAt(j) == needle.charAt(i))
+                j++;
+            next[i] = j;
+        }
+
+        j = 0;
+        for (int i = 0 ; i < haystack.length() ; i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j))
+                j = next[j - 1];
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+                if (j == needle.length())
+                    return i - needle.length() + 1;
             }
         }
-        return - 1;
-
+        return -1;
     }
 }
---
+++ String/src/leetcode_459.java
@@ -0,0 +1,15 @@
+/**
+ *ClassName: leetcode_459
+ *Package: PACKAGE_NAME
+ *Description:创建于 2025/6/3 21:34
+ *@Author lyl
+ *@Version 1.0
+ */
+public class leetcode_459 {
+}
+
+class Solution5 {
+    public boolean repeatedSubstringPattern(String s) {
+        
+    }
+}
