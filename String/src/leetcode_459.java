e254c3aa4acadfff681bf8b80aa15d5074cd3093
lyl <13416088820@163.com>
2025-06-04 21:43:49 +0800

    6-4

--- String/src/leetcode_459.java
+++ String/src/leetcode_459.java
@@ -6,10 +6,27 @@
  *@Version 1.0
  */
 public class leetcode_459 {
+    public static void main (String[] args) {
+        Solution5 solution5 = new Solution5();
+        System.out.println(solution5.repeatedSubstringPattern(new String("abab")));
+    }
 }
 
 class Solution5 {
-    public boolean repeatedSubstringPattern(String s) {
-        
+    public boolean repeatedSubstringPattern (String s) {
+        //暴力解法：
+        for (int i = 0 ; i < s.length() / 2 ; i++) {
+            int len = i + 1;
+            int j;
+            for (j = i + 1; j + len <= s.length() ; j += len) {
+                String s1 = s.substring(0 , i + 1);
+                String s2 = s.substring(j , j + len);
+                if (! s.substring(0 , i + 1).equals(s.substring(j , j + len)))
+                    break;
+            }
+            if (j >= s.length())
+                return true;
+        }
+        return false;
     }
 }
