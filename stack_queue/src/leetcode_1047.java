3bc49814bd0eba5d5767cccad6f202befb146330
lyl <13416088820@163.com>
2025-07-19 22:59:31 +0800

    7-19

--- stack_queue/src/leetcode_1047.java
+++ stack_queue/src/leetcode_1047.java
@@ -6,10 +6,25 @@
  *@Version 1.0
  */
 public class leetcode_1047 {
+    public static void main (String[] args) {
+        Solution1 solution1 = new Solution1();
+        System.out.println(solution1.removeDuplicates("abbaca"));
+    }
 }
 
-class Solution {
-    public String removeDuplicates(String s) {
-
+class Solution1 {
+    public String removeDuplicates (String s) {
+        char[] arr = s.toCharArray();
+        char[] stack = new char[s.length()];
+        int top = - 1;
+        for (int i = 0 ; i < s.length() ; i++) {
+            if (top != - 1 && arr[i] == stack[top]) {
+                top--;
+            } else {
+                stack[++ top] = arr[i];
+            }
+        }
+        String str = new String(stack);
+        return str.substring(0 , top + 1);
     }
 }
---
+++ stack_queue/src/leetcode_150.java
@@ -0,0 +1,55 @@
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+/**
+ *ClassName: leetcode_150
+ *Package: PACKAGE_NAME
+ *Description:创建于 2025/7/19 21:45
+ *@Author lyl
+ *@Version 1.0
+ */
+public class leetcode_150 {
+    public static void main (String[] args) {
+        Solution2 solution2 = new Solution2();
+        System.out.println(solution2.evalRPN(new String[]{"10","6","9","3","+","-11","*","/","*","17","+","5","+"}));
+    }
+
+    @Test
+    public void test(){
+        System.out.println("-11".matches("\\d+"));
+    }
+}
+
+class Solution2 {
+    public int evalRPN (String[] tokens) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0 ; i < tokens.length ; i++) {
+            if (tokens[i].matches("\\d+")) {
+                int x = Integer.parseInt(tokens[i]);
+                stack.push(x);
+            } else {
+                if (tokens[i].equals("+")) {
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.push(b + a);
+                } else if (tokens[i].equals("-")) {
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.push(b - a);
+                } else if (tokens[i].equals("*")) {
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.push(b * a);
+                } else if (tokens[i].equals("/")) {
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.push(b / a);
+                }
+            }
+        }
+        return stack.peek();
+    }
+}
