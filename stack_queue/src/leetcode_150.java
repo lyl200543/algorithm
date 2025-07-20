246b86bc97d916776c75603a237e0073613621ea
lyl <13416088820@163.com>
2025-07-20 21:52:06 +0800

    7-20

--- stack_queue/src/leetcode_150.java
+++ stack_queue/src/leetcode_150.java
@@ -19,37 +19,68 @@ public class leetcode_150 {
 
     @Test
     public void test(){
-        System.out.println("-11".matches("\\d+"));
+        System.out.println("-11".matches("-?\\d+"));
     }
 }
 
+//class Solution2 {
+//    public int evalRPN (String[] tokens) {
+//        Deque<Integer> stack = new ArrayDeque<>();
+//        for (int i = 0 ; i < tokens.length ; i++) {
+//            if (tokens[i].matches("-?\\d+")) {
+//                int x = Integer.parseInt(tokens[i]);
+//                stack.push(x);
+//            } else {
+//                if (tokens[i].equals("+")) {
+//                    int a = stack.pop();
+//                    int b = stack.pop();
+//                    stack.push(b + a);
+//                } else if (tokens[i].equals("-")) {
+//                    int a = stack.pop();
+//                    int b = stack.pop();
+//                    stack.push(b - a);
+//                } else if (tokens[i].equals("*")) {
+//                    int a = stack.pop();
+//                    int b = stack.pop();
+//                    stack.push(b * a);
+//                } else if (tokens[i].equals("/")) {
+//                    int a = stack.pop();
+//                    int b = stack.pop();
+//                    stack.push(b / a);
+//                }
+//            }
+//        }
+//        return stack.peek();
+//    }
+//}
+
+
+//改进：
 class Solution2 {
-    public int evalRPN (String[] tokens) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        for (int i = 0 ; i < tokens.length ; i++) {
-            if (tokens[i].matches("\\d+")) {
-                int x = Integer.parseInt(tokens[i]);
-                stack.push(x);
-            } else {
-                if (tokens[i].equals("+")) {
-                    int a = stack.pop();
-                    int b = stack.pop();
-                    stack.push(b + a);
-                } else if (tokens[i].equals("-")) {
-                    int a = stack.pop();
-                    int b = stack.pop();
-                    stack.push(b - a);
-                } else if (tokens[i].equals("*")) {
-                    int a = stack.pop();
-                    int b = stack.pop();
-                    stack.push(b * a);
-                } else if (tokens[i].equals("/")) {
-                    int a = stack.pop();
-                    int b = stack.pop();
-                    stack.push(b / a);
-                }
-            }
+    int index = 0;
+    public int evalRPN(String[] tokens) {
+        index = tokens.length - 1;
+        return cal(tokens);
+    }
+    public int cal(String[] tokens) {
+        if (tokens[index].equals("*")) {
+            index--;
+            return cal(tokens) * cal(tokens);
+        } else if (tokens[index].equals("/")) {
+            index--;
+            int a = cal(tokens);
+            int b = cal(tokens);
+            return b / a;
+        } else if (tokens[index].equals("+")) {
+            index--;
+            return cal(tokens) + cal(tokens);
+        } else if (tokens[index].equals("-")) {
+            index--;
+            int a = cal(tokens);
+            int b = cal(tokens);
+            return b - a;
+        } else {
+            return Integer.valueOf(tokens[index--]);
         }
-        return stack.peek();
     }
 }
