6f20ea5f1c7bf8f9c4605eb6e7ab06e1ba808b85
lyl <13416088820@163.com>
2025-09-17 19:59:29 +0800

    9-17

--- zuo/src/necessary/class027_3_leetcode_2208.java
+++ zuo/src/necessary/class027_3_leetcode_2208.java
@@ -16,7 +16,47 @@ public class class027_3_leetcode_2208 {
 }
 
 class Solution7 {
-    public int halveArray (int[] nums) {
+    public static int MAXN = 100001;
+    public static long[] heap = new long[MAXN];
+    public static int n;
 
+    public int halveArray (int[] nums) {
+        n = nums.length;
+        long sum = 0;
+        for (int i = n - 1 ; i >= 0 ; i--) {
+            heap[i] = (long) nums[i] << 20;
+            sum += heap[i];
+            heapify(i);
+        }
+        sum = sum / 2;
+        long sub = 0;
+        int count = 0;
+        while (sub < sum) {
+            heap[0] = heap[0] / 2;
+            sub += heap[0];
+            heapify(0);
+            count++;
+        }
+        return count;
+    }
+
+    public static void swap (int a , int b) {
+        long tmp = heap[a];
+        heap[a] = heap[b];
+        heap[b] = tmp;
+    }
+
+    public static void heapify (int start) {
+        int i = start;
+        while (i * 2 + 1 < n) {
+            int left = i * 2 + 1;
+            int best = left + 1 < n && heap[left + 1] > heap[left] ? left + 1 : left;
+            best = heap[i] < heap[best] ? best : i;
+            if (i == best) {
+                break;
+            }
+            swap(i , best);
+            i = best;
+        }
     }
 }
