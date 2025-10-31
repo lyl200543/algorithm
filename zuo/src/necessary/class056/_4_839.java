3f6d68b51f70ff3270a82e534608d9f86108d5b6
lyl <13416088820@163.com>
2025-10-31 21:59:05 +0800

    10-31

--- zuo/src/necessary/class056/_4_839.java
+++ zuo/src/necessary/class056/_4_839.java
@@ -9,22 +9,61 @@ package necessary.class056;
  *@Version 1.0
  */
 public class _4_839 {
+    public static void main(String[] args) {
+//        Solution2 solution2 = new Solution2();
+//        System.out.println(solution2.numSimilarGroups(new String[]{"blw", "bwl", "wlb"}));
+    }
 }
 
-class Solution2 {
+
+class Solution {
     private int[] parent;
+    private int setNum;
 
     public int numSimilarGroups(String[] strs) {
         build(strs.length);
+        int size = strs[0].length();
+        setNum = strs.length;
         for (int i = 0; i < strs.length; i++) {
-            
+            for (int j = i + 1; j < strs.length; j++) {
+                if (find(i) != find(j)) {
+                    int count = 0;
+                    //提高效率：count>2后没必要再遍历
+                    for (int k = 0; k < size && count <= 2; k++) {
+                        if (strs[i].charAt(k) != strs[j].charAt(k)) {
+                            count++;
+                        }
+                    }
+                    if (count <= 2) {
+                        union(i, j);
+                    }
+                }
+            }
         }
+        return setNum;
     }
 
     private void build(int length) {
         parent = new int[length];
         for (int i = 0; i < length; i++) {
             parent[i] = i;
+        }
+    }
+
+    private int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    private void union(int a, int b) {
+        int i = find(a);
+        int j = find(b);
+        if (i != j) {
+            parent[i] = j;
+            setNum--;
         }
     }
 }
---
+++ zuo/src/necessary/class056/_5_200.java
@@ -0,0 +1,18 @@
+package necessary.class056;
+
+/**
+ *ClassName: _5_200
+ *Package: necessary.class056
+ *Description:创建于 2025/10/31 21:23
+ * https://leetcode.cn/problems/number-of-islands/description/
+ *@Author lyl
+ *@Version 1.0
+ */
+public class _5_200 {
+}
+
+class Solution {
+    public int numIslands(char[][] grid) {
+
+    }
+}
