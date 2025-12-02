e20a03480686eb4eded76e87f999772d6a635a34
lyl <13416088820@163.com>
2025-12-02 22:22:48 +0800

    12-2

--- zuo/src/necessary/class063/_1_127.java
+++ zuo/src/necessary/class063/_1_127.java
@@ -1,12 +1,63 @@
 package necessary.class063;
 
+import java.util.HashSet;
+import java.util.List;
+
 /**
  *ClassName: _1_127
  *Package: necessary.class063
  *Description:创建于 2025/11/30 22:00
  * https://leetcode.cn/problems/word-ladder/
+ * 双向广搜：两边同时bfs,每次选跟小的一边bfs，直到相遇
+ * 1.优化：剪枝
  *@Author lyl
  *@Version 1.0
  */
 public class _1_127 {
+}
+class Solution1 {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        HashSet<String> words = new HashSet<>(wordList);
+        if (!words.contains(endWord)) {
+            return 0;
+        }
+        HashSet<String> smallLevel = new HashSet<>();
+        HashSet<String> bigLevel = new HashSet<>();
+        HashSet<String> nextLevel = new HashSet<>();
+        smallLevel.add(beginWord);
+        bigLevel.add(endWord);
+        for (int level = 2; !smallLevel.isEmpty(); level++) {
+            for (String word : smallLevel) {
+                char[] arr = word.toCharArray();
+                for (int i = 0; i < arr.length; i++) {
+                    char old = arr[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c != old) {
+                            arr[i] = c;
+                            String str = String.valueOf(arr);
+                            if (bigLevel.contains(str)) {
+                                return level;
+                            }
+                            if (words.contains(str)) {
+                                words.remove(str);
+                                nextLevel.add(str);
+                            }
+                        }
+                    }
+                    arr[i] = old;
+                }
+            }
+            if (nextLevel.size() <= bigLevel.size()) {
+                HashSet<String> tmp = smallLevel;
+                smallLevel = nextLevel;
+                nextLevel = tmp;
+            } else {
+                smallLevel = bigLevel;
+                bigLevel = nextLevel;
+                nextLevel = new HashSet<>();
+            }
+            nextLevel.clear();
+        }
+        return 0;
+    }
 }
